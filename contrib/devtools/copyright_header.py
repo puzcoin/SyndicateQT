2c40a409c5292e49ed3f65298d331b3a0a07d187
puzcoin <akuma@mud.com.cn>
2018-04-15 05:24:12 -0400

    a

--- contrib/devtools/copyright_header.py
+++ contrib/devtools/copyright_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2016-2017 The Syndicate Core developers
+# Copyright (c) 2016-2017 The Mybasecoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -85,10 +85,10 @@ def compile_copyright_regex(copyright_style, year_style, name):
 
 EXPECTED_HOLDER_NAMES = [
     "Satoshi Nakamoto\n",
-    "The Syndicate Core developers\n",
-    "The Syndicate Core developers \n",
-    "Syndicate Core Developers\n",
-    "the Syndicate Core developers\n",
+    "The Mybasecoin Core developers\n",
+    "The Mybasecoin Core developers \n",
+    "Mybasecoin Core Developers\n",
+    "the Mybasecoin Core developers\n",
     "The Syndicate developers\n",
     "The LevelDB Authors\. All rights reserved\.\n",
     "BitPay Inc\.\n",
@@ -342,7 +342,7 @@ def write_file_lines(filename, file_lines):
 COPYRIGHT = 'Copyright \(c\)'
 YEAR = "20[0-9][0-9]"
 YEAR_RANGE = '(%s)(-%s)?' % (YEAR, YEAR)
-HOLDER = 'The Syndicate Core developers'
+HOLDER = 'The Mybasecoin Core developers'
 UPDATEABLE_LINE_COMPILED = re.compile(' '.join([COPYRIGHT, YEAR_RANGE, HOLDER]))
 
 def get_updatable_copyright_line(file_lines):
@@ -410,24 +410,24 @@ def exec_update_header_year(base_directory):
 ################################################################################
 
 UPDATE_USAGE = """
-Updates all the copyright headers of "The Syndicate Core developers" which were
+Updates all the copyright headers of "The Mybasecoin Core developers" which were
 changed in a year more recent than is listed. For example:
 
-// Copyright (c) <firstYear>-<lastYear> The Syndicate Core developers
+// Copyright (c) <firstYear>-<lastYear> The Mybasecoin Core developers
 
 will be updated to:
 
-// Copyright (c) <firstYear>-<lastModifiedYear> The Syndicate Core developers
+// Copyright (c) <firstYear>-<lastModifiedYear> The Mybasecoin Core developers
 
 where <lastModifiedYear> is obtained from the 'git log' history.
 
 This subcommand also handles copyright headers that have only a single year. In those cases:
 
-// Copyright (c) <year> The Syndicate Core developers
+// Copyright (c) <year> The Mybasecoin Core developers
 
 will be updated to:
 
-// Copyright (c) <year>-<lastModifiedYear> The Syndicate Core developers
+// Copyright (c) <year>-<lastModifiedYear> The Mybasecoin Core developers
 
 where the update is appropriate.
 
@@ -460,7 +460,7 @@ def get_header_lines(header, start_year, end_year):
     return [line + '\n' for line in lines]
 
 CPP_HEADER = '''
-// Copyright (c) %s The Syndicate Core developers
+// Copyright (c) %s The Mybasecoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 '''
@@ -469,7 +469,7 @@ def get_cpp_header_lines_to_insert(start_year, end_year):
     return reversed(get_header_lines(CPP_HEADER, start_year, end_year))
 
 PYTHON_HEADER = '''
-# Copyright (c) %s The Syndicate Core developers
+# Copyright (c) %s The Mybasecoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 '''
@@ -523,7 +523,7 @@ def insert_cpp_header(filename, file_lines, start_year, end_year):
 def exec_insert_header(filename, style):
     file_lines = read_file_lines(filename)
     if file_already_has_core_copyright(file_lines):
-        sys.exit('*** %s already has a copyright by The Syndicate Core developers'
+        sys.exit('*** %s already has a copyright by The Mybasecoin Core developers'
                  % (filename))
     start_year, end_year = get_git_change_year_range(filename)
     if style == 'python':
@@ -536,7 +536,7 @@ def exec_insert_header(filename, style):
 ################################################################################
 
 INSERT_USAGE = """
-Inserts a copyright header for "The Syndicate Core developers" at the top of the
+Inserts a copyright header for "The Mybasecoin Core developers" at the top of the
 file in either Python or C++ style as determined by the file extension. If the
 file is a Python file and it has a '#!' starting the first line, the header is
 inserted in the line below it.
@@ -550,7 +550,7 @@ where <year_introduced> is according to the 'git log' history. If
 
 "<current_year>"
 
-If the file already has a copyright for "The Syndicate Core developers", the
+If the file already has a copyright for "The Mybasecoin Core developers", the
 script will exit.
 
 Usage:
